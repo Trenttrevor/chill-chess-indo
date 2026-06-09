59a67fcab23e621c51f5ddcf881f884379a86b37
trent <trent@trents-MacBook-Air.local>
2026-06-09 20:05:53 +0700

    add start script

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || "3000",
   DATABASE_URL: process.env.DATABASE_URL,
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
     process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
